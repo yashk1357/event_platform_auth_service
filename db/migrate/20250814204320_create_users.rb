f9b11fe5b35bbf868a8645723aee41f73a5bd7fd
yash karray <yashkarray14@gmail.com>
2025-08-15 18:42:24 +0530

    add auth controler to handle user signin and singup

--- db/migrate/20250814204320_create_users.rb
+++ db/migrate/20250814204320_create_users.rb
@@ -3,9 +3,10 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :name
       t.string :email, null: false
-      t.string :provider
-      t.string :password_digest
-      t.string :role
+      t.string :provider, null: true
+      t.string :password_digest, null: false
+      t.string :role, default: 'user'
+      t.string :uid, null: true
 
       t.timestamps
       t.index :email, unique: true
