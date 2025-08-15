991f6e99e229e0a2021001342c465a591593f653
yash karray <yashkarray14@gmail.com>
2025-08-15 17:45:24 +0530

    fix syntax enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    enum: { user: 0, admin: 1, organizer: 2}
+    enum role: { user: 0, admin: 1, organizer: 2}
 
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }, only: :create
