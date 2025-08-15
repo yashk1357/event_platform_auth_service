311e423aac6b81fd3123143eb8e009e9bb72ea1a
yash karray <yashkarray14@gmail.com>
2025-08-15 17:37:44 +0530

    added service for JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
     enum: { user: 0, admin: 1, organizer: 2}
 
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :password, presence: true, length: { minimum: 6 }, only: :create
 end
